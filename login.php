13b0dface7db1806ef5b4b2ea8ac5c135ac69c79
DeadMaka90 <deadmakar90yandex.ru>
2020-04-30 17:09:02 +0300

    comments1

--- login.php
+++ login.php
@@ -1,14 +1,20 @@
 <?php
-require('connect.php');
-$usr = $_POST['username'];
-$pass = $_POST['password'];
-$sql = "SELECT * FROM `USERS` WHERE `LOGIN` = '$usr' AND `PASSWD` = '$pass' AND `FLAG` != 0";
-$result=mysqli_query($conn,$sql);
-$count=mysqli_num_rows($result);
-if($count !=0) {
-	echo "Вход";
+require('connect.php'); //пдключаемся к базе
+$usr = $_POST['username']; //считываение парамера login
+$pass = $_POST['password']; //считываение парамера passwd
+$sql = "SELECT * FROM `USERS` WHERE `LOGIN` = '$usr' AND `PASSWD` = '$pass' AND `FLAG` != 0"; //Запрос к базе данных
+$result=mysqli_query($conn,$sql); //запись результата запроса
+$count=mysqli_num_rows($result); //подсчет кол-ва записей возвращенных запросом
+if ($count!=0) {
+	$_SESSION['username'] = $usr; //если юзер найден в БД,то открываем сессию
+
 }
 else{
-	echo"Доступ запрещен";
+	echo"<script>alert('Доступ запрещен');</script>"; //если флаг 0, то доступ запрещен
+	echo "<script>location.replace('index.php');</script>";
+}
+if(isset($_SESSION['username'])){  //если переменная $_SESSION не пустая, т
+	$usr=$_SESSION['username']; 
+	header('Location: admins_form.php'); //перебрасываем на следующуюю страницу
 }
 ?>
