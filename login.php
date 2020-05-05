a9010436ac086268964bd9f17b5f2a4f076240d0
DeadMakar90 <deadmakar90@yandex.ru>
2020-05-05 14:06:35 +0000

    admin_forms_add

--- login.php
+++ login.php
@@ -7,14 +7,16 @@ $result=mysqli_query($conn,$sql); //запись результата запро
 $count=mysqli_num_rows($result); //подсчет кол-ва записей возвращенных запросом
 if ($count!=0) {
 	$_SESSION['username'] = $usr; //если юзер найден в БД,то открываем сессию
-
 }
 else{
 	echo"<script>alert('Доступ запрещен');</script>"; //если флаг 0, то доступ запрещен
 	echo "<script>location.replace('index.php');</script>";
 }
 if(isset($_SESSION['username'])){  //если переменная $_SESSION не пустая, т
-	$usr=$_SESSION['username']; 
-	header('Location: admins_form.php'); //перебрасываем на следующуюю страницу
+	$usr=$_SESSION['username'];
+	header('Location: user_form.php'); //перебрасываем на следующуюю страницу
+		if ($usr == 'admin'){
+			header('Location: admins_form.php');
+		}
 }
-?>
+
