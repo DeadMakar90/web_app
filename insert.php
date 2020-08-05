8117da1c594f9186d3dcdb4d32f18b404f869faf
deadmakar90 <deadmakar90@yandex.ru>
2020-08-05 18:23:55 +0300

    05.08.20_inser_update

--- insert.php
+++ insert.php
@@ -6,4 +6,5 @@ $access=$_POST['access'];
 $sql= "INSERT INTO `USERS` VALUES (default, '$login', '$pass', '$access')";
 mysqli_query($conn,$sql) or die(mysqli_error($conn));
 echo "<script>alert(\"Пользователь добавлен!\");</script>";
+echo "<script>location.replace('user_insert.php');</script>";
 
