0d59e0dc7411e78f83ec0b5e84ec686d8f8a4240
deadmakar90 <deadmakar90@yandex.ru>
2020-08-05 17:53:46 +0300

    05.08.20

--- insert.php
+++ insert.php
@@ -3,5 +3,5 @@ require('connect.php');
 $login = $_POST['login'];
 $pass=$_POST['pass'];
 $access=$_POST['access'];
-$sql= "INSERT INTO `USERS` VALUES (default, '$login', '$pass', '$access',)";
+$sql= "INSERT INTO `USERS` VALUES (default, '$login', '$pass', '$access')";
 mysqli_query($conn,$sql) or die(mysqli_error($conn));
